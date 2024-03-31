95615a5dee81581edfe2a49c8de0fbc5abf48762
JayeshNaik101 <jayeshnnaik1999@gmail.com>
2024-03-31 17:04:12 -0400

    Adding initial Files

---
+++ Javascript/website/index.js
@@ -0,0 +1,37 @@
+console.log("I like paneer tikka pizza");
+console.log("I love it");
+//window.alert("I really love pizza");
+
+/* 
+-- Variables is a container for storing data 
+-- Variables behaves as if it is the value itself
+
+There are two steps 
+# 1. Declaration (var, let, const)
+# 2. Assignment (= assignment operator)
+
+*/
+
+let firstName = "Jayesh";
+let lastName = "Naik";
+let student = false;
+let age = 23;
+
+age += 2;
+
+console.log(age);
+console.log(firstName +" Jaywant "+ lastName);
+console.log("Enrolled : " + student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + (age+1) + " years old";
+document.getElementById("p3").innerHTML = "Enrolled : " + student;
+
+/*
+   Operator Precidence
+   # Paranthesis
+   # Exponents
+   # Multiplication & Division
+   # Addition & Subtraction
+*/
+/* User Inputs */
